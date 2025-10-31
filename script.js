f79b071f34d45a97d04c02c55ee54e4a4a6bfb05
Ryan <ryansilvadeoliveira53@gmail.com>
2025-10-31 14:26:11 -0300

    adição das functions e themes

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-const iconsun = document.getElementById ( 'iconsun');
-const iconmoon = document.getElementById ( 'iconmoon');
+const iconsun = document.getElementById ('iconsun');
+const iconmoon = document.getElementById ('iconmoon');
+const icon = document.getElementById ('icon');
+
+function isSaturn (){
+    return icon.src.indexOf ('saturn') > -1
+}
 
 function sun () {
-    icon.src = './imagens/sun.png';
+    if (!isSaturn () ) {
+        icon.src = './imagens/sun.png';
+        document.body.classList.remove('dark-theme', 'universe-theme');
+        document.body.classList.add('light-theme');
+    }
 }
 
 function moon () {
-    icon.src = './imagens/moon.png';
+    if (!isSaturn () ) {
+        icon.src = './imagens/moon.png';
+        document.body.classList.remove('light-theme', 'universe-theme');
+        document.body.classList.add('dark-theme');
+    }
+}
+
+function saturn () {
+        icon.src = './imagens/saturn.png';
+        document.body.classList.remove('light-theme', 'dark-theme');
+        document.body.classList.add('universe-theme');
 }
 
 iconsun.addEventListener( 'click', sun );
 iconmoon.addEventListener( 'click', moon);
+icon.addEventListener( 'dblclick', saturn);
